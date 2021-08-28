9f85cfdbf73d60d9ae373d564fc5c45b2e70a37c
rintonorma <rinto456@gmail.com>
2021-08-29 00:39:51 +0700

    signup

--- components/auth/authFooter.vue
+++ components/auth/authFooter.vue
@@ -1,13 +1,53 @@
 <template>
   <div>
-    footer
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="text-center py-4">
+            <nuxt-link to=""><img src="~/static/img/playstore.png"></nuxt-link>
+            <nuxt-link to=""><img src="~/static/img/appstore.png"></nuxt-link>
+          </div>
+        </div>
+        <div class="col-12">
+          <div class="d-flex align-items-center justify-content-around gap-2 mb-4">
+            <nuxt-link to="" class="btn border-0">Ketentuan Layanan</nuxt-link>
+            <nuxt-link to="" class="btn border-0">Kebijakan & Privasi</nuxt-link>
+            <nuxt-link to="" class="btn border-0">Pusat Bantuan</nuxt-link>
+            <nuxt-link to="" class="btn border-0">Tentang Kami</nuxt-link>
+            <nuxt-link to="" class="btn border-0">Kontak</nuxt-link>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="bg-light">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="d-flex align-items-center justify-content-between py-3">
+              <div>&copy; Copyright 2021 LBH HKTI. All right reserved.</div>
+              <div class="d-flex align-items-center gap-2">
+                <nuxt-link to="" class="btn border-0"><Icon icon="ant-design:instagram-filled" style="font-size: 24px;" class=""/></nuxt-link>
+                <nuxt-link to="" class="btn border-0"><Icon icon="akar-icons:twitter-fill" style="font-size: 24px;" class=""/></nuxt-link>
+                <nuxt-link to="" class="btn border-0"><Icon icon="brandico:facebook" style="font-size: 24px;" class=""/></nuxt-link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { Icon } from '@iconify/vue2'
+
 export default {
 
   name: 'authFooter',
+
+  components:{
+    Icon
+  },
 
   data () {
     return {
@@ -17,5 +57,13 @@ export default {
 }
 </script>
 
-<style lang="css" scoped>
+<style lang="scss" scoped>
+a{
+  color: #2A2B2A;
+
+  &:hover{
+    color: #6C757D;
+    text-decoration: none;
+  }
+}
 </style>
--- pages/auth/signup.vue
+++ pages/auth/signup.vue
@@ -1,17 +1,43 @@
 <template>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-12 col-lg-4 offset-lg-4">
-        asdass
+  <div>
+    <div class="bg-auth p-4">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12 col-lg-4 offset-lg-4">
+            <div class="card">
+              <div class="card-body p-5">
+                <div class="text-center mb-4">
+                  <img src="~/static/img/logo.png">
+                </div>
+                <h4 class="text-center mb-4">Lembaga Bantuan Hukum Himpunan Kerukunan Tani Indonesia</h4>
+                <h5 class="text-center mb-4">Sudah punya akun? <nuxt-link to="" class="text-danger">Masuk</nuxt-link></h5>
+                <form>
+                  <label for="input-email" class="text-secondary">Email</label>
+                  <div class="input-group mb-3">
+                    <input type="text" class="form-control form-control-lg" id="input-email" aria-describedby="input-email" placeholder="nama@email.com">
+                  </div>
+                  <div class="text-center mb-4"><small>Dengan mendaftar, saya menyatakan bahwa saya setuju dengan <nuxt-link to="" class="text-danger">Ketentuan Layanan</nuxt-link> dan <nuxt-link to="" class="text-danger">Kebijakan Privasi</nuxt-link></small></div>
+                  <nuxt-link to="" class="btn btn-success btn-lg btn-block">Selanjutnya</nuxt-link>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
+    <AuthFooter />
   </div>
 </template>
 
 <script>
+import AuthFooter from '~/components/auth/authFooter'
 export default {
 
   name: 'signup',
+
+  components:{
+    AuthFooter,
+  },
 
   data () {
     return {
