0aecdd8b445341ada5233db9234dd17f608aa525
rintonorma <rinto456@gmail.com>
2021-08-28 10:06:19 +0700

    update

---
+++ pages/auth/signup.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12 col-lg-4 offset-lg-4">
+        asdass
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+  name: 'signup',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
